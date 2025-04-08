88dc947c5732d48d442481a3a162c58383cb9be3
Grimmauld <Grimmauld@grimmauld.de>
2025-04-08 10:44:50 +0200

    nix: use rust-overlay

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -11,12 +15,17 @@
       self,
       nixpkgs,
       flake-utils,
+      rust-overlay,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
       in
       {
         packages.nix-check-deps = pkgs.rustPlatform.buildRustPackage {
@@ -28,25 +37,18 @@
             lockFile = ./Cargo.lock;
           };
 
-          
           # skips rebuilding the whole thing with debug info
           doCheck = false;
         };
 
-        defaultPackage = self.packages.${system}.nix-check-deps;
-
         devShell = pkgs.mkShell {
-          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-
-          inputsFrom = builtins.attrValues self.packages.${system};
           buildInputs = [
-            pkgs.cargo-outdated
-            pkgs.rustfmt
-            pkgs.clippy
-            pkgs.flamegraph
-            pkgs.nix-eval-jobs
+            rustToolchain
           ];
         };
+
+        defaultPackage = self.packages.${system}.nix-check-deps;
+
       }
     );
 }
