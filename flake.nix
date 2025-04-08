fe34458268a0e7216b904fefd12a28575ddbd35b
Grimmauld <Grimmauld@grimmauld.de>
2025-04-08 10:51:51 +0200

    ci: add nix-github-actions

--- flake.nix
+++ flake.nix
@@ -8,6 +8,10 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-github-actions = {
+      url = "github:nix-community/nix-github-actions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -16,39 +20,49 @@
       nixpkgs,
       flake-utils,
       rust-overlay,
+      nix-github-actions,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-      in
-      {
-        packages.nix-check-deps = pkgs.rustPlatform.buildRustPackage {
-          name = "nix-check-deps";
-          version = "1.0.0";
+    let
+      outputs = flake-utils.lib.eachDefaultSystem (
+        system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        in
+        {
+          packages.nix-check-deps = pkgs.rustPlatform.buildRustPackage {
+            name = "nix-check-deps";
+            version = "1.0.0";
 
-          src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
+            src = ./.;
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+
+            # skips rebuilding the whole thing with debug info
+            doCheck = false;
           };
 
-          # skips rebuilding the whole thing with debug info
-          doCheck = false;
-        };
+          devShell = pkgs.mkShell {
+            buildInputs = [
+              rustToolchain
+            ];
+          };
 
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            rustToolchain
-          ];
-        };
+          defaultPackage = self.packages.${system}.nix-check-deps;
 
-        defaultPackage = self.packages.${system}.nix-check-deps;
+        }
+      );
+    in
+    outputs
+    // {
 
-      }
-    );
+      githubActions = nix-github-actions.lib.mkGithubMatrix {
+        checks = outputs.x86_64.packages;
+      };
+    };
 }
